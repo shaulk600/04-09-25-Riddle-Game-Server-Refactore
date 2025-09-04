c9681f709d903c21e8798099c84c9e9c0b8b26de
Shaul Kimel <shaullk600@gmail.com>
2025-09-04 16:49:52 +0300

    update: start refactore and create a file in name  dalRefactore/

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express";
-import  configRoutes  from "./routes/configRoute.js";
+import { config } from "dotenv";
+config({ path: './.env' });
+import cors from "cors";
+import configRoutes from "./routes/configRoute.js";
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cors());
 
-configRoutes(app); 
 
-const PORT = process.env.PORT || 3000 ;
-app.listen(PORT, ()=> console.log('server to RiddleGame running .. '));
+configRoutes(app);
+
+const PORT = process.env.PORT ;
+app.listen(PORT, () => console.log('server to RiddleGame running .. '));
